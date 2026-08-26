e06b7e26d59756bb8416b95f112fcaa9945b9f50
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-08-26 23:17:33 +0530

    Add genre constant values for movie categorization

---
+++ Frontend/src/constants/genres.js
@@ -0,0 +1,22 @@
+export const GENRES = [
+  { id: 28, name: 'Action' },
+  { id: 35, name: 'Comedy' },
+  { id: 27, name: 'Horror' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Sci-Fi' },
+  { id: 53, name: 'Thriller' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 99, name: 'Documentary' },
+]
+
+export const LANGUAGES = [
+  { code: 'en', name: 'English' },
+  { code: 'hi', name: 'Hindi' },
+  { code: 'ta', name: 'Tamil' },
+  { code: 'es', name: 'Spanish' },
+  { code: 'fr', name: 'French' },
+  { code: 'ja', name: 'Japanese' },
+  { code: 'ko', name: 'Korean' },
+  { code: 'de', name: 'German' },
+]
