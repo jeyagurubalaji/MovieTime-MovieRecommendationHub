224f3724bf8c25c2964a132b64d1e304ba1389a7
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-09-23 23:03:00 +0530

    Add Spring context integration unit test suite

---
+++ Backend/src/test/java/com/movietime/MovieTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movietime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
