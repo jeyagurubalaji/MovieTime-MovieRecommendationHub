9501f960bf8b9a35bef7dbcdc9e3d4e42e7e4f51
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-09-23 23:14:04 +0530

    Add Oscar winner awards static reference data helper

---
+++ Backend/src/main/java/com/movietime/core/OscarWinners.java
@@ -0,0 +1,15 @@
+package com.movietime.core;
+
+import java.util.List;
+
+/**
+ * Dynamic TMDB Curated List IDs for Oscar & Academy Award Winners.
+ * List 28 / 7067626: TMDB Official Oscar Best Picture & Major Award Winners List.
+ */
+public final class OscarWinners {
+
+    // Dynamic TMDB Curated List IDs updated live on TMDB
+    public static final List<String> OSCAR_LIST_IDS = List.of("28", "7067626");
+
+    private OscarWinners() {}
+}
